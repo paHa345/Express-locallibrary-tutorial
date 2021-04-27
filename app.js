b65954abcaf737498895de5e9de58ae7d1322efe
paHa345 <pav.345@mail.ru>
2021-04-27 14:52:55 +0700

    Added files app and package required for deploying

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ var wiki = require('./routes/wiki')
 //mongoose connection
 
 var mongoose = require('mongoose')
-var mongoDB = 'mongodb+srv://paHa:dfger2244@cluster0.w0ukw.mongodb.net/local_library?retryWrites=true&w=majority'
+var dev_db_url = 'mongodb+srv://paHa:dfger2244@cluster0.w0ukw.mongodb.net/local_library?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true})
 var db = mongoose.connection
